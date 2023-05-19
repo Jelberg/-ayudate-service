c11512390012113b85b177e316303168db7bfbde
Jessica Elberg <elbergjessica@gmail.com>
2023-05-19 11:02:19 -0400

    Archivo json de configuracion

--- package/environment.js
+++ package/environment.js
@@ -1,8 +1,9 @@
 import dotenv from "dotenv";
+import config from "../config.json";
 dotenv.config();
 
 export default {
-  port: process.env.SERVER_PORT || 8888,
-  databaseURL: process.env.MONGO_URL,
-  node: process.env.NODE_ENV || "development",
+  port: config.SERVER_PORT || process.env.SERVER_PORT,
+  databaseURL: config.MONGO_URL || process.env.MONGO_URL,
+  node: config.NODE_ENV || process.env.NODE_ENV,
 };
